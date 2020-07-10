e9ed6ab542d9fc6cfb10b719ede4efd7e4226832
Joe Portaro <jsportaro@outlook.com>
2020-07-10 11:25:13 -0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
     loop {}
 }
 
